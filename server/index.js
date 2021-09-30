448556fce4d496d1a3727bcb3b60a8a95e59ec90
Faustino <faustino.maggioni@est.fi.uncoma.edu.ar>
2021-09-30 11:10:39 -0300

    setup server base y api de cryptos

--- server/index.js
+++ server/index.js
@@ -1,2 +1,19 @@
-import http from 'http';
+import express from 'express';
+import CoinGecko from 'coingecko-api';
 
+const app = express();
+const CoinGeckoClient = new CoinGecko();
+
+const func = async() => {
+    let data = await CoinGeckoClient.ping();
+    console.log(data);
+  };
+
+app.get('/', function(req, res) {
+    res.send('Hola Mundo!');
+  });
+  
+  app.listen(3000, function() {
+    console.log('Aplicación ejemplo, escuchando el puerto 3000!');
+    func();
+  });

225198a775a059956d5a52ef0e9dc8bd9e61a5b7
Faustino <faustino.maggioni@est.fi.uncoma.edu.ar>
2021-10-14 19:44:32 -0300

    lucas

--- frontend/services/index.js
+++ frontend/services/index.js
@@ -1,35 +1,39 @@
 const API_URL = 'http://localhost:5000';
 
+const coins = document.querySelector('#container');
 
-
-async function getCoinsInfo(){
+( async function getCoinsInfo(){
     try {
-        console.log(document)
-        const coins = document.querySelector('#container');
         const res = await fetch(`${API_URL}/coins`,{'mode' : 'cors',});
         const data = await res.json();
-        console.log(coins)
+        console.log(data);
+
         data.forEach((coin) => {
+            console.log('coin', coin)
             const card = document.createElement('div');
             card.classList.add('card');
-            console.log(coin)
+            
             const name = coin.name;
             const price = coin.price;
             const image = coin.image;
+
             const title = document.createElement('p');
             title.textContent = name;
+
             const visual_price = document.createElement('p');
-            visual_price.textContent = price;
+            visual_price.classList.add('price');
+            visual_price.textContent = `$${price}`;
+
             const logo = document.createElement('img');
             logo.src = image;
+
             card.appendChild(title);
             card.appendChild(visual_price);
             card.appendChild(logo);
+
             coins.appendChild(card);
         })
     } catch (error) {
         
     } 
-};
-
-getCoinsInfo();
+} )()

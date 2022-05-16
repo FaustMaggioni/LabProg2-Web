61f020ceb54a52869f525bd5d04c9c0299e65f00
maggioniduffy <faustino.maggioni@est.fi.uncoma.edu.ar>
2022-05-16 13:21:05 -0300

    updated url

--- server/public/services/index.js
+++ server/public/services/index.js
@@ -1,75 +1,75 @@
-const todayURL = `http://localhost:5000/index.html`;
+const todayURL = `https://lab-prog2-web.vercel.app/:5000/index.html`;
 
-const coins = document.querySelector('#container');
+const coins = document.querySelector("#container");
 
 const STEP = 10;
 let from = 0;
 let to = from + STEP;
 
-async function getCoinsInfo(fromParam,toParam){
-    try {
-        const res = await fetch(`api/coins/?from=${from}&to=${to}`,{ 
-            'method' : 'GET',
-            'mode' : 'cors',
-        });
+async function getCoinsInfo(fromParam, toParam) {
+  try {
+    const res = await fetch(`api/coins/?from=${from}&to=${to}`, {
+      method: "GET",
+      mode: "cors",
+    });
 
-        from = from + STEP;
-        to = from + STEP;
-        
-        const data = await res.json();
+    from = from + STEP;
+    to = from + STEP;
 
-        data.forEach((coin) => {
-            const card = document.createElement('div');
-            card.classList.add('card');
-            
-            const { name, price, image, id } = getCoinAttributes(coin);
-            card.id = id;
-            const title = document.createElement('p');
-            title.textContent = name;
+    const data = await res.json();
 
-            const visual_price = document.createElement('p');
-            visual_price.classList.add('price');
-            visual_price.textContent = `$${price}`;
+    data.forEach((coin) => {
+      const card = document.createElement("div");
+      card.classList.add("card");
 
-            const logo = document.createElement('img');
-            logo.src = image;
-            logo.classList.add('coin-info')
+      const { name, price, image, id } = getCoinAttributes(coin);
+      card.id = id;
+      const title = document.createElement("p");
+      title.textContent = name;
 
-            appendToCard(card, title, visual_price, logo);
-            setCardListener(card);
+      const visual_price = document.createElement("p");
+      visual_price.classList.add("price");
+      visual_price.textContent = `$${price}`;
 
-            coins.appendChild(card);
-        })
-    } catch (error) {
-        console.log(error)
-    } 
+      const logo = document.createElement("img");
+      logo.src = image;
+      logo.classList.add("coin-info");
+
+      appendToCard(card, title, visual_price, logo);
+      setCardListener(card);
+
+      coins.appendChild(card);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-getCoinsInfo(from,to);
+getCoinsInfo(from, to);
 
-const loadMoreBtn = document.querySelector('#loadmore');
-loadMoreBtn.addEventListener('click', () => getCoinsInfo(from,to))
+const loadMoreBtn = document.querySelector("#loadmore");
+loadMoreBtn.addEventListener("click", () => getCoinsInfo(from, to));
 
-function getCoinAttributes(coin){
-    const isActual = window.location.href === todayURL;
+function getCoinAttributes(coin) {
+  const isActual = window.location.href === todayURL;
 
-    return {
-        name: coin.name,
-        price: isActual ? coin.price : coin.price_last_week,
-        image: coin.image,
-        id: coin.id,
-    }
+  return {
+    name: coin.name,
+    price: isActual ? coin.price : coin.price_last_week,
+    image: coin.image,
+    id: coin.id,
+  };
 }
 
-function appendToCard(card, ...childrens){
-    childrens.forEach((item) => {
-        card.appendChild(item);
-    })
+function appendToCard(card, ...childrens) {
+  childrens.forEach((item) => {
+    card.appendChild(item);
+  });
 }
 
 function setCardListener(card) {
-    card.addEventListener("click", () => {
-        sessionStorage.setItem("id", card.id);
-        window.location.href = `/coin.html`;
-    })
+  card.addEventListener("click", () => {
+    sessionStorage.setItem("id", card.id);
+    window.location.href = `/coin.html`;
+  });
 }

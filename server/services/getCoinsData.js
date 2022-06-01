8cdd71dd827dcb08a0d1e825b5041bf4b2fd0e3a
maggioniduffy <faustino.maggioni@est.fi.uncoma.edu.ar>
2022-06-01 15:58:37 -0300

    ranking

--- server/services/getCoinsData.js
+++ server/services/getCoinsData.js
@@ -1,45 +1,47 @@
-import CoinGecko from 'coingecko-api';
+import CoinGecko from "coingecko-api";
 
 const CoinGeckoClient = new CoinGecko();
 
 const getPriceLastWeek = (current_price, perc) => {
-    return (current_price - current_price * (perc * 0.01)).toFixed(2);
-  }
+  return (current_price - current_price * (perc * 0.01)).toFixed(2);
+};
 
 export const getCoinsData = async () => {
   let { data } = await CoinGeckoClient.coins.all();
-  const res = data.map((coin) => {
+  console.log(Object.keys(data[1]));
+  const res = data.map((coin, i) => {
     const price = coin.market_data.current_price.usd.toFixed(2);
     const percChange7d = coin.market_data.price_change_percentage_7d;
     const priceLastWeek = getPriceLastWeek(price, percChange7d);
-    
-    return(
-      {
-        id: coin.id, 
-        name: coin.name, 
-        symbol: coin.symbol, 
-        price, 
-        image: coin.image.small,
-        price_last_week: priceLastWeek,
-      })
-  })
+    const ranking = i;
+    console.log(ranking);
+    return {
+      id: coin.id,
+      name: coin.name,
+      symbol: coin.symbol,
+      price,
+      image: coin.image.small,
+      price_last_week: priceLastWeek,
+      ranking,
+    };
+  });
   return res;
 };
 
 export const getSingleCoinData = async (id) => {
   let { data } = await CoinGeckoClient.coins.fetch(id);
   if (data.error) {
-    if (data.error === 'Could not find coin with the given id'){
-      throw {...data, code: 404}
+    if (data.error === "Could not find coin with the given id") {
+      throw { ...data, code: 404 };
     }
     throw data;
   }
-  
+
   const price = data.market_data.current_price.usd.toFixed(2);
-  console.log(price)
+  console.log(price);
   const percChange7d = data.market_data.price_change_percentage_7d;
   const priceLastWeek = getPriceLastWeek(price, percChange7d);
-  console.log(priceLastWeek, data.name, data.image.large, data.market_cap_rank)
+  console.log(priceLastWeek, data.name, data.image.large, data.market_cap_rank);
   const res = {
     name: data.name,
     image: data.image.large,
@@ -48,6 +50,6 @@ export const getSingleCoinData = async (id) => {
     description: data.description.en,
     price,
     priceLastWeek,
-  }
+  };
   return res;
-}
+};

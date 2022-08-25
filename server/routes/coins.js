b7d5f5bcb71c39953b88af2db4447f0a7441d72f
maggioniduffy <faustino.maggioni@est.fi.uncoma.edu.ar>
2022-08-25 14:17:48 -0300

    No image needed

--- server/routes/coins.js
+++ server/routes/coins.js
@@ -8,31 +8,16 @@ let coins;
 const DIEZ_MINUTOS = 1000 * 60 * 10;
 
 setInterval(async () => {
-    coins = await getCoinsData();
-  }, DIEZ_MINUTOS);
+  coins = await getCoinsData();
+}, DIEZ_MINUTOS);
 
-router.use( async (req, res, next) => {
-    coins = await getCoinsData();
-    next();
-})
+router.use(async (req, res, next) => {
+  coins = await getCoinsData();
+  next();
+});
 
 router.get("/", async (req, res) => getAllCoins(req, res, coins));
 
 router.get("/:id", async (req, res) => getCoinById(req, res));
 
-router.post("/", async (req, res) => addNewCoin(req,res));
-
-
-//api alternativa
-/*
-app.get("/api/mycoins", async (req, res) => {
-    fs.readFile("../server/data/coins.json", (err, data) => {
-      if (err) {
-        res.status(500).send("error interno del servidor");
-      } else {
-        let coins = JSON.parse(data);
-        console.log(coins);
-        res.status(200).send(coins);
-      }
-    });
-  });*/
+router.post("/", async (req, res) => addNewCoin(req, res));
--- server/routes/helpers/addNewCoin.js
+++ server/routes/helpers/addNewCoin.js
@@ -1,33 +1,35 @@
-import { v4 as makeId } from 'uuid';
+import { v4 as makeId } from "uuid";
 import fs from "fs";
 
-export default function addNewCoin(req,res) {
-    const coin = req.body;
-    const { name, image, price } = coin;
+export default function addNewCoin(req, res) {
+  const coin = req.body;
+  console.log(req.body);
+  const { name, image, price } = coin;
 
-    if (!name || !image || !price) {
-      res.send(404).send("<h1> Credenciales invalidas </h1>");
-      return;
-    }
-
-    fs.readFile("../../server/data/coins.json", (err, coinsJSON) => {
-      if (err) {
-        res.status(500).send("error interno del servidor");
-      } else {
-        const coinsList = JSON.parse(coinsJSON);
-        const newCoin = {...coin, id: makeId()};
-        coinsList.coins.push(newCoin);
-        const coinsListAsString = JSON.stringify(coinsList, null, 2);
-
-        fs.writeFile("../server/data/coins.json", coinsListAsString, (err) => {
-          if (err) {
-            res.status(500).send("error interno del servidor");
-          } else {
-            console.log(`se agrego ${req.body.id} a la lista`);
-            res.status(200).send(req.body);
-          }
-        });
-        
-      }
-    });
+  if (!name || !price) {
+    res.sendStatus(404).send("<h1> Credenciales invalidas </h1>");
+    return;
   }
+
+  fs.readFile("../../data/coins.json", (err, coinsJSON) => {
+    if (err) {
+      console.log("1: ", err.message);
+      res.status(500).send("error interno del servidor");
+    } else {
+      const coinsList = JSON.parse(coinsJSON);
+      const newCoin = { ...coin, id: makeId() };
+      coinsList.coins.push(newCoin);
+      const coinsListAsString = JSON.stringify(coinsList, null, 2);
+
+      fs.writeFile("server/data/coins.json", coinsListAsString, (err) => {
+        if (err) {
+          console.log("2: ", err.message);
+          res.status(500).send("error interno del servidor");
+        } else {
+          console.log(`se agrego ${req.body.id} a la lista`);
+          res.status(200).send(req.body);
+        }
+      });
+    }
+  });
+}
